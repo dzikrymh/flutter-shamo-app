e760f1547e8d7c01ad50938537063accf414b605
Dzikry Maulana Hakim <dzikry.maulana.hakim@gmail.com>
2021-09-30 12:15:11 +0700

    first commit

---
+++ lib/pages/home/main_page.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:shamo/pages/home/chat_page.dart';
+import 'package:shamo/pages/home/favorite_page.dart';
+import 'package:shamo/pages/home/home_page.dart';
+import 'package:shamo/pages/home/profile_page.dart';
+import 'package:shamo/theme.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget cartButton() {
+      return FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: secondaryColor,
+        child: Image.asset(
+          'assets/icon_cart.png',
+          width: 20,
+        ),
+      );
+    }
+
+    Widget customBottomNav() {
+      return ClipRRect(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(30),
+        ),
+        child: BottomAppBar(
+          shape: CircularNotchedRectangle(),
+          notchMargin: 12,
+          clipBehavior: Clip.antiAlias,
+          child: BottomNavigationBar(
+            backgroundColor: backgroundColor4,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            type: BottomNavigationBarType.fixed,
+            currentIndex: currentIndex,
+            onTap: (value) {
+              setState(() {
+                currentIndex = value;
+              });
+            },
+            items: [
+              BottomNavigationBarItem(
+                icon: Container(
+                  margin: EdgeInsets.only(
+                    top: 20,
+                    bottom: 10,
+                  ),
+                  child: Image.asset(
+                    'assets/icon_home.png',
+                    width: 21,
+                    color: currentIndex == 0 ? primaryColor : deactiveColor,
+                  ),
+                ),
+                label: "Home",
+              ),
+              BottomNavigationBarItem(
+                icon: Container(
+                  margin: EdgeInsets.only(
+                    top: 20,
+                    bottom: 10,
+                  ),
+                  child: Image.asset(
+                    'assets/icon_chat.png',
+                    width: 20,
+                    color: currentIndex == 1 ? primaryColor : deactiveColor,
+                  ),
+                ),
+                label: "Chat",
+              ),
+              BottomNavigationBarItem(
+                icon: Container(
+                  margin: EdgeInsets.only(
+                    top: 20,
+                    bottom: 10,
+                  ),
+                  child: Image.asset(
+                    'assets/icon_favorite.png',
+                    width: 20,
+                    color: currentIndex == 2 ? primaryColor : deactiveColor,
+                  ),
+                ),
+                label: "Wishlist",
+              ),
+              BottomNavigationBarItem(
+                icon: Container(
+                  margin: EdgeInsets.only(
+                    top: 20,
+                    bottom: 10,
+                  ),
+                  child: Image.asset(
+                    'assets/icon_profile.png',
+                    width: 18,
+                    color: currentIndex == 3 ? primaryColor : deactiveColor,
+                  ),
+                ),
+                label: "Profile",
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    Widget body() {
+      switch (currentIndex) {
+        case 0:
+          return HomePage();
+          break;
+        case 1:
+          return ChatPage();
+          break;
+        case 2:
+          return FavoritePage();
+          break;
+        case 3:
+          return ProfilePage();
+          break;
+        default:
+          return HomePage();
+      }
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor1,
+      floatingActionButton: cartButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: customBottomNav(),
+      body: Center(
+        child: body(),
+      ),
+    );
+  }
+}
