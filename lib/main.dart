cfa1f50ea6c357848afa2119039dace1ab7e674b
Dzikry <dzikry.maulana.hakim@gmail.com>
2022-11-28 11:53:44 +0700

    add constructor in main

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,11 @@ import 'package:shamo/pages/sign_in_page.dart';
 import 'package:shamo/pages/sign_up_page.dart';
 import 'package:shamo/pages/splash_page.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
